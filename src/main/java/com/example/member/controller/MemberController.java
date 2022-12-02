19f6368088aa0426e5c0fdb51ec46e946d9d2c01
alswjd094 <alswjd094@gmail.com>
2022-12-02 15:50:03 +0900

    상세조회,삭제,수정

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -19,84 +19,120 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/member/save")
-    public String saveForm(){
+    public String saveForm() {
         return "memberPages/memberSave";
     }
+
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         memberService.save(memberDTO);
-        return"memberPages/memberLogin";
-    }
-    @GetMapping("/member/login")
-    public String loginForm(){
         return "memberPages/memberLogin";
     }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "memberPages/memberLogin";
+    }
+
     @PostMapping("/member/login")
-    public String login(@ModelAttribute MemberDTO memberDTO,HttpSession session,Model model){
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session, Model model) {
         MemberDTO loginResult = memberService.login(memberDTO);
-        if(loginResult !=null){
-            session.setAttribute("loginEmail",memberDTO.getMemberEmail());
-            model.addAttribute("modelEmail",memberDTO.getMemberEmail());
+        if (loginResult != null) {
+            session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+            model.addAttribute("modelEmail", memberDTO.getMemberEmail());
             return "memberPages/memberMain";
-        }else{
+        } else {
             return "memberPages/memberLogin";
         }
     }
+
     @GetMapping("/member/logout")
-    public String logout(HttpSession session){
+    public String logout(HttpSession session) {
         session.invalidate();
         return "index";
     }
+
     @GetMapping("/member/")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         List<MemberDTO> memberDTOList = memberService.findAll();
-        model.addAttribute("members",memberDTOList);
+        model.addAttribute("members", memberDTOList);
         return "memberPages/memberList";
     }
+
     @GetMapping("/member/{id}")
-    public String findById(@PathVariable Long id, Model model){
+    public String findById(@PathVariable Long id, Model model) {
         System.out.println("id = " + id);
         MemberDTO memberDTO = memberService.findById(id);
-        model.addAttribute("member",memberDTO);
+        model.addAttribute("member", memberDTO);
         return "memberPages/memberDetail";
     }
 
     @GetMapping("/member/update")
-    public String updateForm(Model model,HttpSession session){
+    public String updateForm(Model model, HttpSession session) {
         String loginEmail = (String) session.getAttribute("loginEmail");
         MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
-        model.addAttribute("member",memberDTO);
+        model.addAttribute("member", memberDTO);
         return "memberPages/memberUpdate";
     }
 
     @PostMapping("/member/update")
-    public String update(@ModelAttribute MemberDTO memberDTO){
+    public String update(@ModelAttribute MemberDTO memberDTO) {
         memberService.update(memberDTO);
-        return"memberPages/memberMain";
+        return "memberPages/memberMain";
     }
 
     @GetMapping("/member/delete/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id) {
         memberService.delete(id);
         return "redirect:/member/";
     }
 
     @PostMapping("/member/dup-check")
 //    public @ResponseBody String emailDuplicateCheck(@RequestParam("inputEmail") String memberEmail){
-    public ResponseEntity emailDuplicateCheck(@RequestParam("inputEmail") String memberEmail){
-          //body뿐만 아니라 headers 정보도 같이 보내줌
+    public ResponseEntity emailDuplicateCheck(@RequestParam("inputEmail") String memberEmail) {
+        //body뿐만 아니라 headers 정보도 같이 보내줌
         System.out.println("memberEmail = " + memberEmail);
         String inputEmailResult = memberService.emailDuplicateCheck(memberEmail);
 //        return inputEmailResult;
-        if(inputEmailResult != null) {
+        if (inputEmailResult != null) {
             return new ResponseEntity<>("사용할 수 있는 이메일입니다.", HttpStatus.OK);
-                                                         //HttpStatus: 오류코드, 에러 핸들링 방식, OK: 200
-        }else {
-            return new ResponseEntity<>("이미 사용중인 이메일입니다.",HttpStatus.CONFLICT);
-                                                  //409오류
+            //HttpStatus: 오류코드, 에러 핸들링 방식, OK: 200
+        } else {
+            return new ResponseEntity<>("이미 사용중인 이메일입니다.", HttpStatus.CONFLICT);
+            //409오류
         }
+    }
+
+    @GetMapping("/member/ajax/{id}")
+//    public @ResponseBody MemberDTO list_ajax(@PathVariable Long id){
+      public ResponseEntity findByIdAxios(@PathVariable Long id){
+        System.out.println("id = " + id);
+        MemberDTO memberDTO = memberService.findById(id);
+        if(memberDTO !=null){
+            return new ResponseEntity<>(memberDTO,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
-
-    
+    }
+/*
+REST API
+get: /member/{id}, 조회
+post: /member/{id}, 저장
+delete: /member/{id}, 삭제
+put: /member/{id}, 수정
+요청 메시지를 전달하는 목적
+ */
+    @DeleteMapping("/member/{id}")
+    public ResponseEntity deleteByAxios(@PathVariable Long id){
+        System.out.println("id = " + id);
+        memberService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PutMapping("/member/{id}")
+    public ResponseEntity updateByAxios(@PathVariable Long id, @RequestBody MemberDTO memberDTO){
+        System.out.println("id = " + id + ", memberDTO = " + memberDTO);
+        memberService.update(memberDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+
